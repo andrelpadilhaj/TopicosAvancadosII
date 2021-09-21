6a81981a9b91740d5bec60cc76ad08ee094d3be2
andrelpadilhaj <45461597+andrelpadilhaj@users.noreply.github.com>
2021-09-21 17:58:10 -0300

    .

---
+++ ProjMVC23082021/ProjMVC23082021/Data/ProjMVC23082021Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProjMVC23082021.Models;
+
+namespace ProjMVC23082021.Data
+{
+    public class ProjMVC23082021Context : DbContext
+    {
+        public ProjMVC23082021Context (DbContextOptions<ProjMVC23082021Context> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProjMVC23082021.Models.Produto> Produtos { get; set; }
+
+        public DbSet<ProjMVC23082021.Models.Funcionario> Funcionarios { get; set; }
+
+        public DbSet<ProjMVC23082021.Models.Fornecedor> Fornecedores { get; set; }
+    }
+}
