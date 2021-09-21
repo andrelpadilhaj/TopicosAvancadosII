e8a68fc797db898a855325db0562d4ac123294fe
andrelpadilhaj <45461597+andrelpadilhaj@users.noreply.github.com>
2021-09-21 17:36:16 -0300

    .

---
+++ ProjMVC23082021/DBAula23082021/dbo/tables/Funcionarios.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Funcionarios] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]     VARCHAR (50)  NOT NULL,
+    [Telefone] VARCHAR (16)  NOT NULL,
+    [Endereco] VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ ProjMVC23082021/DBAula23082021/dbo/tables/Produtos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Produtos] (
+    [Id]         INT          IDENTITY (1, 1) NOT NULL,
+    [Descricao]  VARCHAR (50) NULL,
+    [Valor]      FLOAT (53)   NULL,
+    [DtCadastro] DATETIME     NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
