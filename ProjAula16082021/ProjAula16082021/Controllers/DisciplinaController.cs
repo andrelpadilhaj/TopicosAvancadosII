db2247a75f2fdf26177b84a5662f674cb5c84a0e
andrelpadilhaj <45461597+andrelpadilhaj@users.noreply.github.com>
2021-09-21 13:46:52 -0300

    .

---
+++ ProjAula16082021/ProjAula16082021/Controllers/DisciplinaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjAula16082021;
+using ProjAula16082021.Models;
+
+namespace ProjAula16082021.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        private DBAula16082021Entities db = new DBAula16082021Entities();
+
+        // GET: Disciplina
+        public ActionResult Index()
+        {
+            return View(db.Disciplinas.ToList());
+        }
+
+        // GET: Disciplina/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            if (disciplina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disciplina);
+        }
+
+        // GET: Disciplina/Createv
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Disciplina/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descricao,QtdAlunos,NomeProfessor")] Disciplinas disciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Disciplinas.Add(disciplina);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(disciplina);
+        }
+
+        // GET: Disciplina/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            if (disciplina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disciplina);
+        }
+
+        // POST: Disciplina/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descricao,QtdAlunos,NomeProfessor")] Disciplina disciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(disciplina).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(disciplina);
+        }
+
+        // GET: Disciplina/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            if (disciplina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disciplina);
+        }
+
+        // POST: Disciplina/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Disciplinas disciplina = db.Disciplinas.Find(id);
+            db.Disciplinas.Remove(disciplina);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjAula16082021/ProjAula16082021/Models/Disciplina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjAula16082021.Models
+{
+    public class Disciplina
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int QtdAlunos { get; set; }
+        public string NomeProfessor { get; set; }
+    }
+}
