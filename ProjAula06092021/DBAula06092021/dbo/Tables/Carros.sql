6a1423d52863d090575910b155949a845ec46b1c
andrelpadilhaj <45461597+andrelpadilhaj@users.noreply.github.com>
2021-09-26 21:25:55 -0300

    .

---
+++ ProjAula06092021/DBAula06092021/dbo/Tables/Carros.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Carros]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Nome] VARCHAR(50) NULL, 
+    [Cor] VARCHAR(20) NULL, 
+    [Ano] INT NULL, 
+    [Modelo] INT NULL, 
+    [Placa] VARCHAR(7) NULL
+)
---
+++ ProjAula06092021/DBAula06092021/dbo/Tables/Locacoes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Locacoes]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Descricao] VARCHAR(50) NULL, 
+    [Id_Carro] INT NULL, 
+    CONSTRAINT [FK_Locacoes_Carros] FOREIGN KEY ([Id_Carro]) REFERENCES [Carros]([Id])
+)
